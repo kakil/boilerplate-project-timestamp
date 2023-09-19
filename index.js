70bc5a6acf687bef5e9f80c431b296ca34c5199a
kakil <kakil@me.com>
2023-09-19 12:56:11 +0000

    Updated to complete the micro service.

--- index.js
+++ index.js
@@ -17,6 +17,32 @@ const fs = require('fs');
 const http = require('http');
 const { time } = require('console');
 
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
 
 
 app.get('/api/:param', (req, res) => {
@@ -38,7 +64,7 @@ app.get('/api/:param', (req, res) => {
       unixTimestamp = parseInt(param, 10);
       utcFormattedDate = new Date(unixTimestamp).toUTCString();
     } else {
-      return res.status(400).json({ error: 'Invalid date or timestamp' });
+      return res.status(400).json({ error: 'Invalid Date' });
     }
   }
 
